e888db073d9a6320beabec331e89ab6a35c8bf25
kshrhrhrhr <d34k03a221@icloud.com>
2026-01-10 22:47:24 +0900

    JSONフォーマットの関数化

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,31 @@
 using namespace std;
 using json = nlohmann::json;
 
+string makeStrJsonFormat(string title) {
+    //jsonデータの作成
+    json j;
+    j["embeds"] = json::array({
+        {
+            {"title", title},
+            {"description", "説明文"},
+            {"color", 5814783},
+            {"fields", json::array({
+                {
+                    {"name", "フィールド1"},
+                    {"value", "値1"},
+                    {"inline", true}
+                },
+                {
+                    {"name", "フィールド2"},
+                    {"value", "値2"},
+                    {"inline", true}
+                }
+            })}
+        }
+    });
+    string body = j.dump();
+    return body;
+}
 int main() {
     cout << "Hello, World!" << endl;
     //通信設定・接続状態・結果などをまとめて持つ「操作単位」
@@ -20,14 +45,17 @@ int main() {
         //POSTリクエストの設定
         curl_easy_setopt(hnd, CURLOPT_POST, 1);
         //Url(discordのWebhookURL)の設定
-        curl_easy_setopt(hnd, CURLOPT_URL, "URL");
-        
-        //jsonデータの作成
-        json j;
-        j["content"] = string("Hello! From C++ program To Discord channel ");//+ to_string(time(NULL));
+        curl_easy_setopt(hnd, CURLOPT_URL, "https://discord.com/api/webhooks/1459177925877174465/h_QrpxwXwwMAKWNaHQ8l9Aw4ol_wJPawOieixDnvmFyhnJ6LR5FkIGku1AoHPLDBfn-B");
+        //ヘッダー情報の設定
+        struct curl_slist* headers = nullptr;
+        headers = curl_slist_append(headers, "Content-Type: application/json");
+        curl_easy_setopt(hnd, CURLOPT_HTTPHEADER, headers);
+
+
         //POSTデータの設定
-        curl_easy_setopt(hnd, CURLOPT_POSTFIELDS,"payload_json={\"content\": \"Hello! From C++ program To Discord channel\"}");
-        //curl_easy_setopt(hnd, CURLOPT_POSTFIELDS, j.dump().c_str());
+        //curl_easy_setopt(hnd, CURLOPT_POSTFIELDS,"payload_json={\"content\": \"Hello! From C++ program To Discord channel\"}");
+        string body = makeStrJsonFormat("JSONフォーマットの関数化");
+        curl_easy_setopt(hnd, CURLOPT_POSTFIELDS, body.c_str());
         //データサイズの設定
         curl_easy_setopt(hnd, CURLOPT_POSTFIELDSIZE, -1L);
         //通信の実行
@@ -51,4 +79,16 @@ C++のjsonライブラリ決定版 nlohmann-json
 https://qiita.com/yohm/items/0f389ba5c5de4e2df9cf
 #31 [C/C++] 日付と時間の取得
 https://note.com/brain_up0505/n/n3da8b5a584a9
+discord webhook リファレンス
+https://discord.com/developers/docs/resources/webhook
+*/
+/*
+{
+  "content": "メッセージ本文",
+  "username": "表示名",
+  "avatar_url": "https://example.com/icon.png",
+  "tts": false,
+  "embeds": []
+}
+
 */
