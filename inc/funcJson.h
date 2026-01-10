cddbe70a852e4d59d7af5e8db270e7ecefb3331e
kshrhrhrhr <d34k03a221@icloud.com>
2026-01-10 23:42:50 +0900

    ヘッダファイルがコミット漏れ

---
+++ inc/funcJson.h
@@ -0,0 +1,13 @@
+#include <string>
+namespace funcJson{
+    std::string makeStrJsonFormat(std::string title);
+}
+/*
+{
+  "content": "メッセージ本文",
+  "username": "表示名",
+  "avatar_url": "https://example.com/icon.png",
+  "tts": false,
+  "embeds": []
+}
+*/
---
+++ inc/funcWebhook.h
@@ -0,0 +1,4 @@
+
+namespace funcWebhook{
+    void sendMessage();
+}
